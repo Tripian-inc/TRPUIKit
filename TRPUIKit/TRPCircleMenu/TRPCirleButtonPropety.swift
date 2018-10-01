fcc2d9d1f7f20202d0b4abd25696e652c1ebc376
Evren Yaşar <evrenyasar@evren-macbook-pro.local>
2018-10-01 10:22:01 +0300

    Add circleButtonProperty

---
+++ TRPUIKit/TRPCircleMenu/TRPCirleButtonPropety.swift
@@ -0,0 +1,18 @@
+//
+//  TRPCirleButtonPropety.swift
+//  TRPUIKit
+//
+//  Created by Evren Yaşar on 28.09.2018.
+//  Copyright © 2018 Evren Yaşar. All rights reserved.
+//
+
+import Foundation
+public struct TRPCirleButtonPropety {
+    public var name:String
+    
+    public var iconName: String
+    public init(name: String, iconName: String) {
+        self.name = name
+        self.iconName = iconName
+    }
+}
