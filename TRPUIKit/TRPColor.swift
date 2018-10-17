0682ce1a56dd4e8bf6cf07f4292331a640496b02
Necati evren Yaşar <necatievren@icloud.com>
2018-10-17 17:11:33 +0300

    Added MyTrip

--- TRPUIKit/TRPColor.swift
+++ TRPUIKit/TRPColor.swift
@@ -11,7 +11,7 @@ public struct TRPColor {
     public static let pink = UIColor(red: 229.0/255.0, green: 78.0/255.0, blue: 83.0/255.0, alpha: 1.0);
     public static let lightGrey = UIColor(red: 200.0/255.0, green: 199.0/255.0, blue: 204.0/255.0, alpha: 1.0);
     public static let darkGrey = UIColor(red: 86.0/255.0, green: 90.0/255.0, blue: 92.0/255.0, alpha: 1.0);
-    
+    public static let lightBlue = UIColor(red: 239.0/255.0, green: 239.0/255.0, blue: 244.0/255.0, alpha: 1.0);
     public static var successMessageColor : UIColor = UIColor(red: 140.0/255, green: 184.0/255, blue: 64.0/255,  alpha: 0.95)
     public static var warningMessageColor : UIColor = UIColor(red: 231.0/255, green: 188.0/255, blue: 1.0/255,   alpha: 0.95)
     public static var errorMessageColor : UIColor = UIColor(red: 218.0/255, green: 35.0/255,  blue: 27.0/255,  alpha: 0.70)
